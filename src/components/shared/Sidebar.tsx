a357eab06ee0c72a39fb0b93d5494e9a59525927
sebalp100 <sebalp100@gmail.com>
2024-02-27 21:48:41 -0300

    Add initial sidebar links

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -1,7 +1,61 @@
+'use client';
+
+import { navLinks } from '@/constants/layout';
+import { SignedIn } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 const Sidebar = () => {
-  return <aside className="sidebar">Sidebar</aside>;
+  const pathname = usePathname();
+
+  return (
+    <aside className="sidebar">
+      <div className="flex size-full flex-col gap-4">
+        <Link href="/" className="sidebar-logo">
+          <Image
+            src="/assets/images/logo-text.svg"
+            alt="Logo"
+            width={180}
+            height={28}
+          />
+        </Link>
+
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.map((link) => {
+                const isActive = link.route === pathname;
+
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? 'bg-purple-gradient text-white'
+                        : 'text-gray-700'
+                    }`}
+                  >
+                    <Link className="sidebar-link" href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && 'brightness-200'}`}
+                      />
+                    </Link>
+                    {link.label}
+                  </li>
+                );
+              })}
+            </ul>
+          </SignedIn>
+        </nav>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
